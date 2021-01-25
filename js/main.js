0317bc10edb193446e53d527293dcbc7b0597abc
arkeyve <arkeyve@gmail.com>
2021-01-26 02:46:54 +0530

    added handtrack.js

--- js/main.js
+++ js/main.js
@@ -13,6 +13,62 @@
     const gainMax = 1;
     const freqMax = 750;
 
+    const fps = 30;
+    const videoInterval = (1/fps) * 1000;
+
+    var webcam = null;
+    var canvas = null;
+    var ctx = null;
+
+    const modelParams = {
+        flipHorizontal: true, // flip e.g for video
+        maxNumBoxes: 1, // maximum number of boxes to detect
+        iouThreshold: 0.5, // ioU threshold for non-max suppression
+        scoreThreshold: 0.6, // confidence threshold for predictions.
+    }
+
+    function launchWebcam() {
+        canvas = document.getElementById("canvas");
+        ctx = canvas.getContext("2d");
+        var canvasHeight = canvas.parentElement.offsetHeight;
+        var canvasWidth = canvas.parentElement.offsetWidth;
+        canvas.setAttribute("height", canvasHeight);
+        canvas.setAttribute("width", canvasWidth);
+
+        webcam = document.getElementById("webcam");
+        webcam.setAttribute("height", canvasHeight);
+        webcam.setAttribute("width", canvasWidth);
+
+        handTrack.startVideo(webcam).then(status => {
+            if(status) {
+                handTrack.load(modelParams).then(model => {
+                    if(!playing) {
+                        startTheremin();
+                    }
+                    predict(model);
+                });
+            }
+        });
+
+        enableBtns();
+    }
+
+    function predict(model) {
+        model.detect(webcam).then(predictions => {
+            // console.log(predictions);
+            if(predictions[0]) {
+                let midX = predictions[0].bbox[0] + (predictions[0].bbox[2] / 2)
+                let midY = predictions[0].bbox[1] + (predictions[0].bbox[2] / 2)
+                playTheremin(midX * freqMul, gainMax - (midY * gainMul));
+            }
+            model.renderPredictions(predictions, canvas, ctx, webcam);
+
+            setTimeout(() => {
+                predict(model);
+            }, 10);
+        });
+    }
+
     function enableBtns() {
         thereminBox = document.getElementById("theremin-box");
         freqMul = freqMax / thereminBox.offsetWidth;
@@ -24,13 +80,13 @@
             }
         });
 
-        thereminBox.addEventListener("mousemove", function(e) {
-            playTheremin(e.offsetX * freqMul, gainMax - (e.offsetY * gainMul));
-        });
-
-        thereminBox.addEventListener("mouseout", function(e) {
-            stopTheremin();
-        })
+        // thereminBox.addEventListener("mousemove", function(e) {
+        //     playTheremin(e.offsetX * freqMul, gainMax - (e.offsetY * gainMul));
+        // });
+        //
+        // thereminBox.addEventListener("mouseout", function(e) {
+        //     stopTheremin();
+        // });
     }
 
     function startTheremin() {
@@ -58,6 +114,6 @@
     }
 
     window.addEventListener("load", function() {
-        enableBtns();
+        launchWebcam();
     });
 })();
